cf149ade97ab9c557c01c5e11f32b9e00fe9805e
zzxming <zzxming77@163.com>
2024-02-21 18:12:36 +0800

    fix: 开发热更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import gulp from 'gulp';
-import postcss from 'gulp-postcss';
 import less from 'gulp-less';
+import postcss from 'gulp-postcss';
+import pxtorem from 'postcss-pxtorem';
+import autoprefixer from 'autoprefixer';
 import cleanCSS from 'gulp-clean-css';
 import consola from 'consola';
 import rename from 'gulp-rename';
-import { resolve } from 'path';
 import { rollup } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import svg from 'rollup-plugin-svg-import';
@@ -13,17 +16,26 @@ import terser from '@rollup/plugin-terser';
 
 const { src, watch, dest, series, parallel } = gulp;
 
-const distBundle = resolve('./dist');
-const demoRoot = resolve('./demo');
-const moduleRoot = resolve('./src');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const distBundle = resolve(__dirname, './dist');
+const demoRoot = resolve(__dirname, './demo');
+const moduleRoot = resolve(__dirname, './src');
 const themeInput = resolve(moduleRoot, 'assets/style/index.less');
 const moduleInput = resolve(moduleRoot, 'index.js');
 const demoInput = resolve(moduleRoot, 'demo.js');
 
 const buildTheme = () => {
+    const postcssPlugins = [
+        autoprefixer(),
+        pxtorem({
+            rootValue: 16,
+            propList: ['*'],
+            selectorBlackList: ['*-origin'],
+        }),
+    ];
     return src(themeInput)
         .pipe(less())
-        .pipe(postcss())
+        .pipe(postcss(postcssPlugins))
         .pipe(
             cleanCSS({}, (details) => {
                 consola.success(
@@ -62,7 +74,7 @@ const buildModule = async () => {
         sourcemap: false,
     });
 };
-const buildDemo = series(buildModule, async () => {
+const buildDemo = async () => {
     const bundle = await rollup({
         input: demoInput,
         treeshake: true,
@@ -83,11 +95,12 @@ const buildDemo = series(buildModule, async () => {
         format: 'iife',
         sourcemap: true,
     });
-});
-const build = parallel(buildTheme, buildDemo);
+};
+const build = parallel(buildTheme, series(buildModule, buildDemo));
 
 export const theme = buildTheme;
 export const module = buildModule;
+export const demo = buildDemo;
 export const dev = () => {
     watch('./src/**/*.js', build);
 };
