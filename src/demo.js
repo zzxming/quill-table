ff3b6fdedd851565591423313e004b7089b67b7e
zzxming <zzxming77@163.com>
2024-08-06 09:30:04 +0800

    style: clear unnecessary code

--- src/demo.js
+++ src/demo.js
@@ -1,63 +1,9 @@
 import Quill from 'quill';
 import TableModule, { rewirteFormats } from './index';
-const Toolbar = Quill.import('modules/toolbar');
-const Icons = Quill.import('ui/icons');
-
-class ToolbarRewrite extends Toolbar {
-    bindFormatSelect = () => {
-        const range = this.quill.getSelection();
-        if (range.length === 0) return;
-        this.rangeFormat = this.quill.getFormat(range);
-        this.quill.isFormat = true;
-        this.quill.on(Quill.events.SELECTION_CHANGE, this.formatRange);
-        setTimeout(() => {
-            this.formaterBtn.classList.add('ql-active');
-        }, 0);
-    };
-
-    unbindFormatSelect = () => {
-        this.quill.off(Quill.events.SELECTION_CHANGE, this.formatRange);
-        this.rangeFormat = undefined;
-        this.formaterBtn.classList.remove('ql-active');
-        this.quill.isFormat = false;
-    };
-
-    formatRange = (range) => {
-        console.log(range);
-        console.log(this.rangeFormat);
-        this.quill.removeFormat(range.index, range.length);
-        for (const format in this.rangeFormat) {
-            this.quill.format(format, this.rangeFormat[format]);
-        }
-        this.unbindFormatSelect();
-    };
-
-    constructor(quill, options) {
-        super(quill, options);
-        this.formaterBtn = this.controls.find(([name]) => name === 'formater')?.[1];
-    }
-}
-
-ToolbarRewrite.moduleName = 'toolbar';
-ToolbarRewrite.DEFAULTS = {
-    ...Toolbar.DEFAULTS,
-};
-ToolbarRewrite.DEFAULTS.handlers.formater = function () {
-    if (this.quill.isFormat) {
-        this.unbindFormatSelect();
-        return;
-    }
-    this.bindFormatSelect();
-};
-
-Icons[
-    'formater'
-] = `<svg viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="4"><path stroke-linejoin="round" d="M8 24h32v18H8zM4 13h14V6h12v7h14v11H4z"/><path d="M16 32v10"/></g></svg>`;
 
 Quill.register(
     {
         [`modules/${TableModule.moduleName}`]: TableModule,
-        [`modules/${ToolbarRewrite.moduleName}`]: ToolbarRewrite,
     },
     true
 );
@@ -80,7 +26,6 @@ const quill = new Quill('#editor', {
             [{ align: ['', 'center', 'right', 'justify'] }],
             ['clean'],
             ['image', 'video'],
-            ['formater'],
 
             [{ table: [] }],
         ],
